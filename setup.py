fe06bc5b7f1a35cdf5952a62bd4ac467cb994e90
Raphael Michel <michel@rami.io>
2023-05-19 15:12:04 +0200

    Convert project to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
-import os
-from distutils.command.build import build
-from pretix_fontpackfree import __version__
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-
-try:
-    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
-        long_description = f.read()
-except:
-    long_description = ''
-
-
-setup(
-    name='pretix-fontpack-free',
-    version=__version__,
-    description='Pack of free fonts for pretix\' ticket editor',
-    long_description=long_description,
-    url='https://github.com/pretix/pretix-fontpack-free',
-    author='Raphael Michel',
-    author_email='mail@raphaelmichel.de',
-    license='Apache Software License',
-
-    install_requires=[],
-    packages=find_packages(exclude=['tests', 'tests.*']),
-    include_package_data=True,
-    entry_points="""
-[pretix.plugin]
-pretix_fontpackfree=pretix_fontpackfree:PretixPluginMeta
-""",
-)
+setup()
